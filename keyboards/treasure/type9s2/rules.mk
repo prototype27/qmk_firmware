4e526e3933136af26346824f3825b7c5527b3266
Kwang Ketcham <prototype27+github@prototype27.com>
2022-02-02 19:05:58 -0800

    Enable mouse keys and VIA for Treasure Type-9 Series II

--- keyboards/treasure/type9s2/rules.mk
+++ keyboards/treasure/type9s2/rules.mk
@@ -8,7 +8,7 @@ BOOTLOADER = atmel-dfu
 #   change yes to no to disable
 #
 BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = no        # Mouse keys
+MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
@@ -17,3 +17,4 @@ BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
 LTO_ENABLE = yes
+VIA_ENABLE = yes
